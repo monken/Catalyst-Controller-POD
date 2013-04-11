52059c9d0e21fb3dc956b2c393fee4299a74ca30
doggerdude <steve.schreiner@ttbsoftware.com>
2013-04-11 16:38:15 -0300

    add gray style to template

--- lib/Catalyst/Controller/POD/Template.pm
+++ lib/Catalyst/Controller/POD/Template.pm
@@ -14,10 +14,12 @@ sub get {
     <title>$title</title>
 
     <link rel="stylesheet" type="text/css" href="$root/ext/resources/css/ext-all.css" />
+    <link rel="stylesheet" type="text/css" href="$root/ext/resources/css/xtheme-gray.css" />
     <link rel="stylesheet" href="$root/cpan.css" type="text/css" />
     <link rel="stylesheet" href="$root/docs.css" type="text/css" />
     <script type="text/javascript" src="$root/ext/adapter/ext/ext-base.js"></script>
     <script type="text/javascript" src="$root/ext/ext-all.js"></script>
+    <script type="text/javascript" >Ext.BLANK_IMAGE_URL = '$root/ext/resources/images/gray/s.gif';</script>
     <link href="$root/prettify/prettify.css" type="text/css" rel="stylesheet" />
     <script type="text/javascript" src="$root/prettify/prettify.js"></script>
     </head>
