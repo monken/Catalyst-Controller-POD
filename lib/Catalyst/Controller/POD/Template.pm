69d8d5fe51c33bc912a80b598da6c4d66482eee4
doggerdude <steve.schreiner@ttbsoftware.com>
2013-04-11 16:23:18 -0300

    Fix IE issues

--- lib/Catalyst/Controller/POD/Template.pm
+++ lib/Catalyst/Controller/POD/Template.pm
@@ -10,6 +10,7 @@ sub get {
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>$title</title>
 
     <link rel="stylesheet" type="text/css" href="$root/ext/resources/css/ext-all.css" />
